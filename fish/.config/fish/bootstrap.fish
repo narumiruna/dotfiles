4f163ff3ddeb2877d2fdf1d92ea8eed45b6e8720
narumi <toucans-cutouts0f@icloud.com>
2026-01-20 21:51:19 +0800

    Refactor Fish configuration: consolidate fnm setup and remove redundant lines

---
+++ fish/.config/fish/bootstrap.fish
@@ -0,0 +1,23 @@
+if not type -q uv
+    cargo install --locked uv
+end
+
+if not type -q atuin
+    cargo install --locked atuin
+end
+
+if not type -q zoxide
+    cargo install --locked zoxide
+end
+
+if not type -q starship
+   cargo install --locked starship
+end
+
+if not type -q fnm
+    cargo install --locked fnm
+end
+
+if not type -q pyenv
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,19 +1,8 @@
+pyenv init - fish | source
+fnm env --use-on-cd --shell fish | source
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-
-    # https://docs.atuin.sh/cli/guide/installation/
-    # curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
     atuin init fish --disable-up-arrow | source
-
-    # https://github.com/ajeetdsouza/zoxide
-    # cargo install zoxide --locked
-    zoxide init fish | source
-
-    # https://starship.rs/guide/
-    # cargo install starship --locked
     starship init fish | source
-
-    # phttps://github.com/pyenv/pyenv
-    pyenv init - fish | source
+    zoxide init fish | source
 end
-
