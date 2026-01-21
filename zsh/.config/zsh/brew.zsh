e4aeb58ca22d22dc4b3765ce4ca0d462f92d7aff
narumi <toucans-cutouts0f@icloud.com>
2026-01-21 14:23:24 +0800

    refactor(brew.zsh): extract functions for macOS checks and path prepending

--- zsh/.config/zsh/brew.zsh
+++ zsh/.config/zsh/brew.zsh
@@ -1,9 +1,18 @@
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  brew_bin="/opt/homebrew/bin/brew"
+is_macos() {
+  [[ "$(uname -s)" == "Darwin" ]]
+}
+
+prepend_path_if_exists() {
+  local dir="$1"
+  [[ -d "$dir" ]] && export PATH="$dir:$PATH"
+}
+
+if is_macos; then
+  local brew_bin="/opt/homebrew/bin/brew"
 
   [[ -x "$brew_bin" ]] || return 0
   eval "$("$brew_bin" shellenv)"
 
-  [[ -d "/opt/homebrew/opt/mysql-client/bin" ]] && export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
-  [[ -d "/opt/homebrew/opt/openjdk/bin" ]] && export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+  prepend_path_if_exists "/opt/homebrew/opt/mysql-client/bin"
+  prepend_path_if_exists "/opt/homebrew/opt/openjdk/bin"
 fi
