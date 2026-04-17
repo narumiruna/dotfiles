686b4c45faaabffdeecadb458ec91352d9cf2118
narumi <toucans-cutouts0f@icloud.com>
2026-04-17 20:36:16 +0800

    fix: reorganize path additions in fish configuration files

--- fish/.config/fish/conf.d/fnm.fish
+++ fish/.config/fish/conf.d/fnm.fish
@@ -1,1 +1,3 @@
+fish_add_path -m $HOME/.cargo/bin
+
 fnm env --use-on-cd --shell fish | source
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,8 @@
 # disable the default greeting
 set -g fish_greeting ""
 
+fish_add_path -m $HOME/.local/bin $HOME/.cargo/bin
+
 if status is-interactive
     starship init fish | source
     zoxide init fish | source
