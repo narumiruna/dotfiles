504679942ce731ea7d76942d3849089425dbde99
narumi <toucans-cutouts0f@icloud.com>
2026-05-12 10:39:18 +0800

    feat: update bun.fish and config.fish to improve Bun path configuration

--- fish/.config/fish/conf.d/bun.fish
+++ fish/.config/fish/conf.d/bun.fish
@@ -1,4 +1,2 @@
 set --export BUN_INSTALL "$HOME/.bun"
-
-# set --export PATH $BUN_INSTALL/bin $PATH
-fish_add_path -m $BUN_INSTALL/bin
+set --export PATH $BUN_INSTALL/bin $PATH
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 # disable the default greeting
 set -g fish_greeting ""
 
-fish_add_path -m $HOME/.local/bin $HOME/.cargo/bin
+fish_add_path -m $HOME/.local/bin $HOME/.cargo/bin $HOME/.bun/bin
 
 if status is-interactive
     starship init fish | source
