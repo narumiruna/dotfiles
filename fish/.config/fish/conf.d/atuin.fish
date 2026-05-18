c7020e5bac854f9865ae3b18c889e6254f78f88e
narumi <toucans-cutouts0f@icloud.com>
2026-05-18 16:00:34 +0800

    fix: add checks for atuin directory and command in atuin.fish

--- fish/.config/fish/conf.d/atuin.fish
+++ fish/.config/fish/conf.d/atuin.fish
@@ -1,5 +1,8 @@
+test -d $HOME/.atuin/bin; or return
+
 fish_add_path -m $HOME/.atuin/bin
 
 if status is-interactive
+    type -q atuin; or return
     atuin init fish --disable-up-arrow | source
 end
