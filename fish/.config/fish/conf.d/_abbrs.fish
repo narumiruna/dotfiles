94e243fc27ac6cdb240186efd6b42c63c9e31a97
narumi <toucans-cutouts0f@icloud.com>
2026-07-30 14:35:35 +0800

    feat: add abbreviation for switching to main branch and pulling updates

--- fish/.config/fish/conf.d/_abbrs.fish
+++ fish/.config/fish/conf.d/_abbrs.fish
@@ -12,6 +12,7 @@ abbr -a gd 'git diff'
 abbr -a gds 'git diff --staged'
 abbr -a gs 'git switch'
 abbr -a gp 'git pull'
+abbr -a gsgp 'git switch main && git pull origin main'
 
 abbr -a extract 'ouch decompress -y'
 abbr -a compress 'ouch compress -y'
